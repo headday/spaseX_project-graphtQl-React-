c0c5a92020d593e2b96f05c2cfec9d55d9706bdb
kirill <svezhinovkirill17@gmail.com>
2021-02-11 22:07:36 +0500

    add launch item

--- client/src/components/LunchItem.js
+++ client/src/components/LunchItem.js
@@ -7,7 +7,7 @@ const LaunchItem = (props) => {
         <div className="row">
             <div className="col-md-9">
             <h4>{props.name}</h4>
-            <p>{props.success} {props.date_local}</p>
+            <p className={props.success === true ? 'text-success' : 'text-danger'}>{props.date_local}</p>
             </div>
             <div className="col-md-3">
                 <button className="btn btn-secondary">Details</button>
